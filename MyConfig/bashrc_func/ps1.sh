08ce55de08758855dad1bf61689c78d73f0ca895
chiebot_hq <huqiong_c@163.com>
2019-11-21 19:53:58 +0800

    bashrc_func用来放一些辅助函数

---
+++ MyConfig/bashrc_func/ps1.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# >>> 修改终端显示 <<<-----------------------------------------------------------------------------------------------
+# 缩短路径显示 
+function shortwd() {
+    num_dirs=3
+    pwd_symbol="..."
+    newPWD="${PWD/#$HOME/~}"
+    if [ $(echo -n $newPWD | awk -F '/' '{print NF}') -gt $num_dirs ]; then
+        newPWD=$(echo -n $newPWD | awk -F '/' '{print $1 "/.../" $(NF-1) "/" $(NF)}')
+    fi 
+    echo -n $newPWD
+}
+# 添加git分支显示
+function git_branch() {
+    git symbolic-ref HEAD --short
+}
+
+function _fish_collapsed_pwd() {
+    local pwd="$1"
+    local home="$HOME"
+    local size=${#home}
+    [[ $# == 0 ]] && pwd="$PWD"
+    [[ -z "$pwd" ]] && return
+    if [[ "$pwd" == "/" ]]; then
+        echo "/"
+        return
+    elif [[ "$pwd" == "$home" ]]; then
+        echo "~"
+        return
+    fi
+    [[ "$pwd" == "$home/"* ]] && pwd="~${pwd:$size}"
+    if [[ -n "$BASH_VERSION" ]]; then
+        local IFS="/"
+        local elements=($pwd)
+        local length=${#elements[@]}
+        for ((i=0;i<length-1;i++)); do
+            local elem=${elements[$i]}
+            if [[ ${#elem} -gt 1 ]]; then
+                elements[$i]=${elem:0:1}
+            fi
+        done
+    else
+        local elements=("${(s:/:)pwd}")
+        local length=${#elements}
+        for i in {1..$((length-1))}; do
+            local elem=${elements[$i]}
+            if [[ ${#elem} > 1 ]]; then
+                elements[$i]=${elem[1]}
+            fi
+        done
+    fi
+    local IFS="/"
+    echo "${elements[*]}"
+}
+
+#export current_conda_env=$CONDA_PROMPT_MODIFIER
+# >>> 修改终端显示 <<<-------------------------------------------------------------------------------------------------
+function changeps(){
+    export PS1='\e[38;5;211m$(_fish_collapsed_pwd)\e[38;5;48m [$(git_branch)]\e[0m$'
+    conda activate base
+}
+
