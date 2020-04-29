bef9b04d0a34f3aa6b183b016534fe57cf1152df
chiebot_hq <huqiong_c@163.com>
2020-04-29 18:13:13 +0800

    新增3个切换conda下载源的函数

--- MyConfig/bashrc_func/utils.sh
+++ MyConfig/bashrc_func/utils.sh
@@ -26,6 +26,43 @@ function condaswitch() {
 	unset CONDAENV_TEMP
 }
 
+#切换conda下载源到清华源
+function condachannel2qh() {
+	source ~/anaconda3/etc/profile.d/conda.sh
+	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r/
+	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/pro/
+	conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ 
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/simpleitk/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/bioconda/
+    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/menpo/
+    conda config --set show_channel_urls yes
+}
+
+#切换conda下载源到中科大
+function condachannel2zkd() {
+	source ~/anaconda3/etc/profile.d/conda.sh
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
+    conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
+    conda config --set show_channel_urls yes
+}
+
+#切换conda下载源到默认
+function condachannel2default() {
+	source ~/anaconda3/etc/profile.d/conda.sh
+	conda config --remove-key channels
+}
+
+
+
 #交互式进入当前目录下的第一层子目录
 function cdd() {
 	export DIR_TEMP=`ls -l|rg "^d"|awk '{print $NF}'|fzf`
